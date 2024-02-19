1327a374dac02c217322a527902ebcf05c46b853
Fellipev <fellipe2reis@gmail.com>
2024-02-19 09:47:22 -0300

    criando a tela de login

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sistema</Text>
+      <Text style={styles.title}>comanda</Text>
+      <TextInput style={styles.input} placeholder='e-mail'/>
+      <TextInput style={styles.input} placeholder='senha' secureTextEntry/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fcfbff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#A253F1',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  input: {
+    padding: 8,
+    width:200,
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginTop: 20,
+    borderRadius: 4
+  }
 });
